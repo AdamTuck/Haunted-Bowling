bb485aa1943d90948d17f076bc5e42d06cc04e3d
Adam Tuck <adamtuck@gmail.com>
2023-10-27 15:35:55 -0700

    Bowling audio added

--- Assets/Scripts/BallCollisions.cs
+++ Assets/Scripts/BallCollisions.cs
@@ -6,11 +6,14 @@ public class BallCollisions : MonoBehaviour
 {
     GameManager gameManager;
     PlayerController playerController;
+    AudioSource ballRollAudio;
+    AudioSource pinHitAudio;
 
     private void Start()
     {
         gameManager = FindObjectOfType<GameManager>();
         playerController = FindObjectOfType<PlayerController>();
+        ballRollAudio = GetComponent<AudioSource>();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -18,6 +21,8 @@ public class BallCollisions : MonoBehaviour
         if (collision.gameObject.CompareTag("Pin"))
         {
             //Debug.Log("Hit Pin: " + collision.gameObject.name);
+            pinHitAudio = collision.gameObject.GetComponent<AudioSource>();
+            pinHitAudio.Play();
         }
     }
 
@@ -26,6 +31,8 @@ public class BallCollisions : MonoBehaviour
         if (other.gameObject.name == "Pit Trigger")
         {
             playerController.throwCompleted = true;
+
+            ballRollAudio.Stop();
         }
     }
 }
